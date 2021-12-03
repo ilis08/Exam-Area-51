94793c49b0846f3bf43f5ff24b5f92997b0526d1
Iliya Taudd <iliya2002pl.ru@gmail.com>
2021-12-03 23:32:16 +0300

    c

---
+++ ExamArea51/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamArea51
+{
+    public enum SecurityLevel
+    {
+        Confidential,
+        Secret,
+        TopSecret
+    }
+
+    public class Agent
+    {
+        public SecurityLevel Level { get; set; }
+
+    }
+}
---
+++ ExamArea51/Elevator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamArea51
+{
+    public enum Floors
+    {
+        G,
+        S, 
+        T1,
+        T2
+    }
+
+    public enum State
+    {
+        Waiting,
+        Moving
+    }
+
+    public class Elevator
+    {
+        private Floors currentFloor;
+        private State state;
+        private List<Agent> agenstInside;
+
+    }
+}
